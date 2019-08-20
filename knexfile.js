b9e7db57f2a16c5fc54354fd700815378714999f
Maxime Salomon <maxime@croissant.io>
2019-08-20 17:21:23 +0200

    Knex migrate organizations

---
+++ knexfile.js
@@ -0,0 +1,49 @@
+module.exports = {
+
+  development: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'db_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+  test: {
+    client: 'postgresql',
+    connection: 'postgres://postgres:root@127.0.0.1:7777/bravo_test',
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'db_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+  production: {
+    client: 'postgresql',
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      directory: './data/migrations',
+      tableName: 'db_migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
+  },
+
+}; 

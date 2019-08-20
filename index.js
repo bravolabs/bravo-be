789812debfef79fc3e90626a3df5d53ad27d9506
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-20 11:48:30 +0100

    Chore: Setup a basic entry file for the backend application

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { config } from 'dotenv';
+config();
+
+import bot from './bot';
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/bot', bot);
+app.listen(port, () => {
+  console.log(`running on ${port}`);
+});

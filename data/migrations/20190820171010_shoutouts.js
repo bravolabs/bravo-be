509c344f5bea2dafb4c612965cbec59e27e49422
Maxime Salomon <maxime@croissant.io>
2019-08-20 18:27:34 +0200

    Knex migrate shoutouts

--- data/migrations/20190820171010_shoutouts.js
+++ data/migrations/20190820171010_shoutouts.js
@@ -1,8 +1,25 @@
-
 exports.up = function(knex) {
-  
-};
+  return knex.schema.createTable('shoutouts', tbl => {
+    tbl.increments()
+    tbl
+      .text('giver_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    tbl
+      .text('receiver_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    tbl.text('message').notNullable()
+    tbl.timestamps(false, true)
+  })
+}
 
 exports.down = function(knex) {
-  
-};
+  return knex.schema.dropTableIfExists('shoutouts')
+}

81227980a83a27c169683c70c348368c348e93ca
Maxime Salomon <maxime@croissant.io>
2019-08-20 18:07:59 +0200

    Knex migrate users

--- data/migrations/20190820170616_users.js
+++ data/migrations/20190820170616_users.js
@@ -1,8 +1,22 @@
-
 exports.up = function(knex) {
-  
-};
+  return knex.schema.createTable('users', tbl => {
+    tbl.increments()
+    tbl
+      .text('org_id')
+      .notNullable()
+      .references('id')
+      .inTable('organizations')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE')
+    tbl
+      .text('slack_mem_id')
+      .unique()
+      .notNullable()
+    tbl.text('email')
+    tbl.text('name')
+  })
+}
 
 exports.down = function(knex) {
-  
-};
+  return knex.schema.dropTableIfExists('users')
+}

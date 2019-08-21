1928d5d9e4b3086701e19551741144b2903e5a91
erozonachi <erozonachi.james@gmail.com>
2019-08-21 22:05:54 +0100

    Add create function to organizations model

--- data/models/organizations.js
+++ data/models/organizations.js
@@ -1,5 +1,17 @@
 const db = require('../dbConfig');
 
 module.exports = {
-
+  create: function(organization) {
+    return db('organizations')
+      .insert(organization)
+      .whereNotExists(function() {
+        this.select('*')
+          .from('organizations')
+          .whereRaw('slack_org_id = ? OR name = ?', [ 
+            organization.slack_org_id,
+            organization.name
+          ]);
+      })
+      .returning('*');
+  }
 }

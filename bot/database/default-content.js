5196033ea0dcc53da9c98300c3c54be95597c713
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-21 13:43:59 +0100

    Chore: Create default content for messages

---
+++ bot/database/default-content.js
@@ -0,0 +1,84 @@
+const dialog = trigger_id => {
+  return {
+    token: process.env.slack_app_token,
+    trigger_id,
+    dialog: JSON.stringify({
+      title: 'Send Shoutout',
+      callback_id: 'send-shoutout',
+      submit_label: 'Submit',
+      elements: [
+        {
+          label: 'Recipient',
+          type: 'select',
+          name: 'Recipient',
+          optional: false,
+          data_source: 'users',
+        },
+        {
+          label: 'ShoutOut',
+          type: 'textarea',
+          name: 'ShoutOut',
+          optional: false,
+        },
+      ],
+    }),
+  };
+};
+
+const shoutIntro = channel_id => {
+  return {
+    channel: channel_id,
+    token: process.env.slack_app_token,
+    attachments: JSON.stringify([
+      {
+        fallback: 'You are unable to choose',
+        callback_id: 'shoutout_key',
+        attachment_type: 'default',
+        title: 'Shoutout Options',
+        text: 'kindly select an option for your bot task',
+        color: '#7ed692',
+        divider: true,
+        actions: [
+          {
+            name: 'Give Shoutout',
+            text: 'Give Shoutout',
+            type: 'button',
+            value: 'give',
+            style: 'default',
+          },
+          // {
+          //   name: 'Retrieve Shoutouts',
+          //   text: 'Retrieve Shoutouts',
+          //   type: 'button',
+          //   value: 'retrieve',
+          //   style: 'default',
+          // },
+        ],
+      },
+    ]),
+  };
+};
+
+const formSubmissionMessage = (channel_id, user_name) => {
+  return {
+    channel: channel_id,
+    token: process.env.slack_app_token,
+    text: 'Successful Form Submission',
+    attachments: JSON.stringify([
+      {
+        fallback: 'You are unable to choose',
+        callback_id: 'successful_submission',
+        attachment_type: 'default',
+        text: `Shoutout sent successfully to @${user_name}`,
+        color: '#7ed692',
+        divider: true,
+      },
+    ]),
+  };
+};
+
+module.exports = {
+  dialog,
+  shoutIntro,
+  formSubmissionMessage,
+};

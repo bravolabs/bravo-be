dc2e78d413bcc9eb704418339fce0ca0c61337b0
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-21 13:43:15 +0100

    Feat: Implement the shoutout bot to display interactive buttons

--- bot/controllers/commands.js
+++ bot/controllers/commands.js
@@ -1,9 +1,26 @@
 const express = require('express');
-
+const axios = require('axios');
+const qs = require('qs');
 const router = express.Router();
+const data = require('../database/default-content');
+const slack = require('../database/slack');
 
-router.all('/commands', async (req, res) => {
-  res.send('boyoyo');
-});
+const commands = async (req, res) => {
+  const keyword = req.body.text;
+  const { channel_id } = req.body;
+  // slack gives 3seconds to respond to a command, before it thinks you failed, and so we are getting operation_timed out error
+  if (keyword === 'shoutout') {
+    const message = data.shoutIntro(channel_id);
+    await slack.postMessage(message);
+  } else if (keyword === 'feedback') {
+    res.send('feedback');
+  } else if (keyword === 'help') {
+    res.send('help');
+  } else {
+    console.log('bad');
+  }
+};
 
-module.exports = router;
+module.exports = {
+  commands,
+};

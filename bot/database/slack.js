ce4a25c99ef53f9c57f67f7e2f9649ad3526e9d2
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-22 10:30:33 +0100

    Feat: Add a layer for making request to the slack api

--- bot/database/slack.js
+++ bot/database/slack.js
@@ -10,7 +10,15 @@ const dialog = async dialog => {
   await axios.post(`${apiUrl}/dialog.open`, qs.stringify(dialog));
 };
 
+const getUser = async user_id => {
+  const response = await axios.get(
+    `${apiUrl}/users.info?token=${process.env.slack_app_token}&user=${user_id}`
+  );
+  return response.data.user;
+};
+
 module.exports = {
   postMessage,
   dialog,
+  getUser,
 };

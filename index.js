cf6d46bf38b87d1922f67c18ff2835dcc7982721
erozonachi <erozonachi.james@gmail.com>
2019-08-22 17:11:06 +0100

    Use shoutouts route in the app entry file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const bot = require('./bot');
+const shoutouts = require('./routes/shoutouts');
 
 const port = process.env.PORT || 3000;
 const app = express();
@@ -10,7 +10,9 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.use('/bot', bot);
+app.use('/api/bot', bot);
+app.use('/api/shoutouts', shoutouts)
+
 app.listen(port, () => {
   console.log(`running on ${port}`);
 });

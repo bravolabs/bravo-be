9ac6dd0592b8af9b449009a8326dacdedded0186
erozonachi <erozonachi.james@gmail.com>
2019-08-22 10:57:47 +0100

    Refien create organizations unit test

--- data/dbModels/organizations.spec.js
+++ data/dbModels/organizations.spec.js
@@ -6,36 +6,12 @@ beforeEach(async () => {
 });
 
 describe('Create organizations', () => {
-  it('can create organizations', () => {
-    orgs.read()
-    .then(result => {
-      expect(result).toHaveLength(0);
-    });
-
-    orgs.create({
-      slack_org_id: 'AQYENEKWQS',
-      name: 'Lambda-School' 
-    })
-    .then(() => orgs.create({
-      slack_org_id: 'SWYEUJGHDR',
-      name: 'Bravo-Labs' 
-    }))
-    .then(() => orgs.read())
-    .then(result => {
-      expect(result).toHaveLength(2);
-    });
-  });
-
-  it('can create organizations correctly', () => {
+  it('can create organizations', async () => {
     
-    orgs.read()
-    .then(result => {
-      expect(result).toHaveLength(0);
-    });
-    //let organizations = await orgs.read();
-    //expect(organizations).toHaveLength(0);
+    let organizations = await orgs.read();
+    expect(organizations).toHaveLength(0);
 
-    /* await orgs.create({
+    await orgs.create({
       slack_org_id: 'AQYENEKWQS',
       name: 'Lambda-School' 
     });
@@ -43,39 +19,38 @@ describe('Create organizations', () => {
       slack_org_id: 'SWYEUJGHDR',
       name: 'Bravo-Labs' 
     });
-    organizations = await orgs.read(); */
-    orgs.create({
-      slack_org_id: 'AQYENEKWQS',
-      name: 'Lambda-School' 
-    })
-    .then(() => orgs.create({
-      slack_org_id: 'SWYEUJGHDR',
-      name: 'Bravo-Labs' 
-    }))
-    .then(() => orgs.read())
-    .then(result => {
-      expect(result[0].name).toBe('Lambda-School');
-      expect(result[1].name).toBe('Bravo-Labs');
-    });
+    organizations = await orgs.read();
+    expect(organizations).toHaveLength(2);
   });
 
-  it('returns the newly created organization', () => {
-    /* const organization = await orgs.create({
-      slack_org_id: 'SWYEUJGHDR',
-      name: 'Bravo-Labs' 
-    }); */
-    orgs.create({
+  it('can create organizations correctly', async () => {
+    
+    let organizations = await orgs.read();
+    expect(organizations).toHaveLength(0);
+
+    await orgs.create({
       slack_org_id: 'AQYENEKWQS',
       name: 'Lambda-School' 
-    })
-    .then(result => {
-      expect(result[0].name).toBe('Lambda-School');
     });
-    //expect(organization[0].name).toBe('Bravo-Labs');
+    await orgs.create({
+      slack_org_id: 'SWYEUJGHDR',
+      name: 'Bravo-Labs' 
+    });
+    organizations = await orgs.read();
+    expect(organizations[0].name).toBe('Lambda-School');
+    expect(organizations[1].name).toBe('Bravo-Labs');
   });
 
-  it('cannot duplicate an organization', () => {
-    /* await orgs.create({
+  it('returns the newly created organization', async () => {
+    const organization = await orgs.create({
+      slack_org_id: 'SWYEUJGHDR',
+      name: 'Bravo-Labs' 
+    });
+    expect(organization[0].name).toBe('Bravo-Labs');
+  });
+
+  it('cannot duplicate an organization', async () => {
+    await orgs.create({
       slack_org_id: 'SWYEUJGHDR',
       name: 'Bravo-Labs' 
     });
@@ -83,17 +58,6 @@ describe('Create organizations', () => {
       slack_org_id: 'SWYEUJGHDR',
       name: 'Bravo-Labs' 
     });
-    expect(organization[0].name).toBe('Bravo-Labs'); */
-    orgs.create({
-      slack_org_id: 'SWYEUJGHDR',
-      name: 'Bravo-Labs' 
-    })
-    .then(() => orgs.create({
-      slack_org_id: 'SWYEUJGHDR',
-      name: 'Bravo-Labs' 
-    }))
-    .then(result => {
-      expect(result[0].name).toBe('Bravo-Labs');
-    });
+    expect(organization[0].name).toBe('Bravo-Labs'); 
   });
 });

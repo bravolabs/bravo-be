07bb0a6881aeea3b0229c93e79d40c08ce544ec6
erozonachi <erozonachi.james@gmail.com>
2019-08-23 11:10:20 +0100

    Refactor routes and services for shoutouts

--- routes/shoutouts.js
+++ routes/shoutouts.js
@@ -3,6 +3,18 @@ const service = require('../services/shoutouts');
 
 const router = express.Router();
 
-router.get('/:userId', service.getShoutouts);
+router.get('/:userId', async (req, res, next) => {
+  try {
+      const { userId } = req.params;
+      const result = await service.getShoutouts(userId);
+      res.status(result.statusCode)
+        .json(result.data);
+  } catch(error) {
+    res.status(500)
+      .json({
+        error: 'server error'
+      });
+  }
+});
 
 module.exports = router;
--- services/shoutouts.js
+++ services/shoutouts.js
@@ -1,34 +1,31 @@
 const users = require('../data/dbModels/users');
 const shoutouts = require('../data/dbModels/shoutouts');
 module.exports = {
-  getShoutouts: async function(req, res, next) {
-    try {
-        const { userId } = req.params;
-        const user = await users.readBySlackId(userId);
-        if(!user || !user.slack_mem_id) {
-          return res.status(404)
-            .json({
-              message: 'User not found'
-            });
+  getShoutouts: async function(userId) {
+    const user = await users.readBySlackId(userId);
+    if(!user || !user.slack_mem_id) {
+      return {
+        statusCode: 404,
+        data: {
+          message: 'User not found'
         }
+      }
+    }
 
-        const result = await shoutouts.read(userId);
-        if(result.length < 1) {
-          return res.status(404)
-            .json({
-              message: 'No shoutouts found for the user'
-            });
+    const result = await shoutouts.read(userId);
+    if(result.length < 1) {
+      return {
+        statusCode: 404,
+        data: {
+          message: 'No shoutouts found for the user'
         }
-        res.status(200)
-          .json({
-            data: result
-          });
-    } catch(error) {
-      console.log(error);
-      res.status(500)
-        .json({
-          error: 'server error'
-        })
+      }
+    }
+    return {
+      statusCode: 200,
+      data: {
+        data: result
+      }
     }
   }
 }

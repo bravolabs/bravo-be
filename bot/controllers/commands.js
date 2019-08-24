b071581741a96df01f57ae242b9baa4e670b0de8
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-24 06:31:48 +0100

    Feat: Create channel the first time the command is ran

--- bot/controllers/commands.js
+++ bot/controllers/commands.js
@@ -14,8 +14,21 @@ const commands = async (req, res) => {
   if (keyword === 'shoutout') {
     const message = await data.shoutIntro(channel_id, user_id);
     await slack.postMessage(message);
+    const channels = await slack.getAllChannels();
+    const channel = await slack.findChannel(process.env.DESIGNATED_CHANNEL, channels.channels);
+
+    // Creating the channel bravo would talk to
+    if (channel.length !== 0) {
+      console.log('channel exists');
+    } else {
+      await slack.createChannel(process.env.DESIGNATED_CHANNEL);
+    }
   } else if (keyword === 'feedback') {
-    console.log('feedback');
+    // console.log('feedback');
+    const res = await axios.get(
+      `https://slack.com/api/users.list?token=${process.env.slack_app_token}&team=TMK9G16SU`
+    );
+    console.log(res.data);
   } else if (keyword === 'help') {
     res.send('help');
   } else {

69db00f5ad2eebef4a9e5673b0b90429dc500d24
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-24 06:32:43 +0100

    Chore: Create a create channel slack nethod

--- bot/database/slack.js
+++ bot/database/slack.js
@@ -10,6 +10,39 @@ const postMessage = async message => {
   }
 };
 
+const createChannel = async name => {
+  try {
+    await axios.post(
+      `${apiUrl}/channels.create`,
+      qs.stringify({
+        token: process.env.slack_app_token,
+        name,
+        validate: true,
+      })
+    );
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const findChannel = async (channel_name, channels) => {
+  try {
+    const channel = channels.filter(singleChannel => singleChannel.name === channel_name);
+    return channel;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getAllChannels = async () => {
+  try {
+    const res = await axios.get(`${apiUrl}/channels.list?token=${process.env.slack_app_token}`);
+    return res.data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
 const postChannel = async message => {
   await axios.post(`${apiUrl}/chat.postMessage`, qs.stringify(message));
 };
@@ -27,7 +60,10 @@ const getUser = async user_id => {
 
 module.exports = {
   postMessage,
+  createChannel,
   postChannel,
+  findChannel,
+  getAllChannels,
   dialog,
   getUser,
 };

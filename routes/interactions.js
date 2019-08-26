551c9369d891c1ce08df2862abeace4c226fcdde
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-26 20:07:21 +0100

    Chore: Implement the interactions route using the necessary services when its time

--- routes/interactions.js
+++ routes/interactions.js
@@ -9,14 +9,23 @@ router.post('/', async (req, res) => {
   try {
     await res.status(200).send('');
     const data = JSON.parse(req.body.payload);
-    console.log(data);
+    // console.log(data);
     if (data.type === 'interactive_message' && data.callback_id === 'shoutout') {
       const reqInfo = {
         actions: data.actions,
-        trigger_id: data.trigger_id,
+        triggerId: data.trigger_id,
       };
 
       await shoutOutService.respondToInteractiveMessage(reqInfo);
+    } else if (data.type === 'dialog_submission' && data.callback_id === 'shoutout') {
+      const reqInfo = {
+        channelId: data.channel.id,
+        userId: data.user.id,
+        recipient: data.submission.Recipient,
+        content: data.submission.ShoutOut,
+      };
+
+      await shoutOutService.submitDialog(reqInfo);
     }
   } catch (err) {
     console.log(err);

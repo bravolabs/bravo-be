135f2e7bd87c630f80ea0bc9e8e999e74dd7650f
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-26 18:50:16 +0100

    Chore: Implement the dialog submission method

--- services/slackServices/shoutout.js
+++ services/slackServices/shoutout.js
@@ -1,5 +1,7 @@
 const { slackModel } = require('../../data/slackModels/slack');
 const { slack } = require('../../config');
+const events = require('events');
+const eventEmitter = new events.EventEmitter();
 
 exports.sendShoutOut = async message => {
   try {
@@ -27,6 +29,39 @@ exports.respondToInteractiveMessage = async (actions, dialog) => {
     } else if (actions[0].value === 'retrieve') {
       console.log('retrieve');
     }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.submitDialog = async reqInfo => {
+  try {
+    const message = {
+      channel: reqInfo.channel_id,
+      user: reqInfo.user_id,
+      text: `You have sent a shoutout to <@${reqInfo.user_name}> 🙌🙌`,
+      token: slack.slackToken,
+      attachments: JSON.stringify([
+        {
+          callback_id: 'submitDialog',
+          attachment_type: 'default',
+          title: 'Shoutout:',
+          text: `${reqInfo.content}`,
+          color: '#7ed692',
+        },
+      ]),
+    };
+
+    await slackModel.postMessage(message);
+
+    // Add an event emitter to emit an event once their is a successful shoutout, so the
+    // listeners can take over and send the necessary notifications
+
+    eventEmitter.emit('successful_shoutout', {
+      user: reqInfo.user_id,
+      recipient: reqInfo.user_name,
+      message: reqInfo.content,
+    });
   } catch (err) {
     console.log(err);
   }

384a6043d57e137441e61905824411e6bdc66ef1
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-26 20:06:50 +0100

    Chore: Add route for interactions

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const interactions = require('./interactions');
 function routes(app) {
   app.use('/api/shoutouts', shoutouts);
   app.use('/slack/command', commands);
-  // app.use('/slack/interactions', interactions);
+  app.use('/slack/interactions', interactions);
 }
 
 module.exports = routes;

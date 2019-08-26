443ae647d2232f4fa9f2c5452c892947a16fce92
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-26 14:56:47 +0100

    Chore: Create config variables, these are variables which are mostly constant through out the application and are also environment variables

---
+++ config/index.js
@@ -0,0 +1,15 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+module.exports = {
+  port: process.env.PORT,
+  slack: {
+    slackToken: process.env.SLACK_APP_TOKEN,
+    bravoChannel: process.env.BRAVO_CHANNEL,
+    verificationToken: process.env.VERIFICATION_TOKEN,
+    clientId: process.env.SLACK_CLIENT_ID,
+    clientSecret: process.env.SLACK_CLIENT_SECRET,
+    baseUrl: process.env.SLACK_BASE_URL,
+  },
+};

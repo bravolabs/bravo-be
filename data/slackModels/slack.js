def21aea16b453a6352c69d38161900fa536e16b
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-26 20:27:28 +0100

    Chore: export all independently created slack models

--- data/slackModels/slack.js
+++ data/slackModels/slack.js
@@ -1,73 +1,12 @@
 const axios = require('axios');
 const qs = require('qs');
 const { slack } = require('../../config');
+const user = require('./user');
+const message = require('./message');
+const channel = require('./channel');
 
 exports.slackModel = {
-  async postMessage(message) {
-    try {
-      await axios.post(`${slack.baseUrl}/chat.postEphemeral`, qs.stringify(message));
-    } catch (err) {
-      console.log(err);
-    }
-  },
-
-  async createChannel(name) {
-    try {
-      await axios.post(
-        `${slack.baseUrl}/channels.create`,
-        qs.stringify({
-          token: process.env.slack_app_token,
-          name,
-          validate: true,
-        })
-      );
-    } catch (err) {
-      console.log(err);
-    }
-  },
-
-  async findChannel(channel_name, channels) {
-    try {
-      const channel = channels.filter(singleChannel => singleChannel.name === channel_name);
-      return channel;
-    } catch (err) {
-      console.log(err);
-    }
-  },
-
-  async getAllChannels() {
-    try {
-      const res = await axios.get(`${slack.baseUrl}/channels.list?token=${slack.slackToken}`);
-      return res.data;
-    } catch (err) {
-      console.log(err);
-    }
-  },
-
-  async postOpenMessage(message) {
-    try {
-      await axios.post(`${slack.baseUrl}/chat.postMessage`, qs.stringify(message));
-    } catch (err) {
-      console.log(err);
-    }
-  },
-
-  async createDialog(dialog) {
-    try {
-      await axios.post(`${slack.baseUrl}/dialog.open`, qs.stringify(dialog));
-    } catch (err) {
-      console.log(err);
-    }
-  },
-
-  async getUser(userId) {
-    try {
-      const response = await axios.get(
-        `${slack.baseUrl}/users.info?token=${slack.slackToken}&user=${userId}`
-      );
-      return response.data.user;
-    } catch (err) {
-      console.log(err);
-    }
-  },
+  user,
+  message,
+  channel,
 };

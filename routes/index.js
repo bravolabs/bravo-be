6cf7b928287f185e26bff3e217b515d012167c68
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-26 16:01:02 +0100

    Chore: Update route list with the route to commands

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,11 @@
 const shoutouts = require('./shoutouts');
+const commands = require('./commands');
+const interactions = require('./interactions');
 
 function routes(app) {
   app.use('/api/shoutouts', shoutouts);
+  app.use('/slack/command', commands);
+  // app.use('/slack/interactions', interactions);
 }
 
 module.exports = routes;

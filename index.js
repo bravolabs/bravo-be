98c540f05b68dd18d99a4d501dc17088680e4ad7
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-27 11:29:14 +0100

    Fix: Refactor code base to have a single express and server file

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
+const { port } = require('./config');
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 routes(app);
+
+app.listen(port, () => {
+  console.log(`running on ${port}`);
+});
 
 module.exports = app;

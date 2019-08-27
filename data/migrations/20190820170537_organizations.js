96454cf962d1dbeb0d12e45bcad727f738178c14
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-27 12:55:03 +0100

    Chore: Refactor the organiozation migration file

--- data/migrations/20190820170537_organizations.js
+++ data/migrations/20190820170537_organizations.js
@@ -1,17 +1,21 @@
 exports.up = function(knex) {
   return knex.schema.createTable('organizations', tbl => {
-    tbl.increments()
+    tbl.increments();
     tbl
       .text('slack_org_id')
       .unique()
-      .notNullable()
+      .notNullable();
     tbl
       .text('name')
       .unique()
-      .notNullable()
-  })
-}
+      .notNullable();
+    tbl.text('channel_name').notNullable();
+    tbl.text('channel_id').notNullable();
+    tbl.text('access_token').notNullable();
+    tbl.timestamp('installation_date').defaultTo(knex.fn.now());
+  });
+};
 
 exports.down = function(knex) {
-  return knex.schema.dropTableIfExists('organizations')
-}
+  return knex.schema.dropTableIfExists('organizations');
+};

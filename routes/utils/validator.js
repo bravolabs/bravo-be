d09c8c7b5cefebaade9594674a220fda02199aa3
erozonachi <erozonachi.james@gmail.com>
2019-08-27 14:08:57 +0100

    Add and export function to validate login payload

--- routes/utils/validator.js
+++ routes/utils/validator.js
@@ -1,3 +1,20 @@
-module.exports = {
+function validateLoginCredentials(req, res, next) {
+  const { accessToken, userId, } = req.body;
+  if(!accessToken || accessToken.trim() === '') {
+    return res.status(400)
+      .json({
+        message: 'Missing required accessToken field'
+      });
+  }
+  if(!userId || userId.trim() === '') {
+    return res.status(400)
+      .json({
+        message: 'Missing required userId field'
+      });
+  }
+  next();
+}
 
+module.exports = {
+  validateLoginCredentials,
 }

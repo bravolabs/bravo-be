489d9989999434706458a888c325ce7db82abf44
erozonachi <erozonachi.james@gmail.com>
2019-08-27 14:46:17 +0100

    Require and use auths route in routes entry file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const shoutouts = require('./shoutouts');
+const auths = require('./auths');
 const commands = require('./commands');
 const interactions = require('./interactions');
 
 function routes(app) {
   app.use('/api/shoutouts', shoutouts);
+  app.use('/api/auths', auths);
   app.use('/slack/command', commands);
   app.use('/slack/interactions', interactions);
 }

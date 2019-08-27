0f638bc277c01a4effc2c7819d3f47af49000e1b
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-27 15:06:06 +0100

    Chore: Remove redundant config variables

--- config/index.js
+++ config/index.js
@@ -6,11 +6,9 @@ module.exports = {
   port: process.env.PORT || 3000,
   slack: {
     slackToken: process.env.SLACK_APP_TOKEN,
-    bravoChannel: process.env.BRAVO_CHANNEL,
     verificationToken: process.env.VERIFICATION_TOKEN,
     clientId: process.env.SLACK_CLIENT_ID,
     clientSecret: process.env.SLACK_CLIENT_SECRET,
     baseUrl: process.env.SLACK_BASE_URL,
-    designatedChannel: process.env.DESIGNATED_CHANNEL,
   },
 };

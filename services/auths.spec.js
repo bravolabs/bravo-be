24243b246fd40e9f163e681b8b6fde6be3c81a14
erozonachi <erozonachi.james@gmail.com>
2019-08-27 15:06:48 +0100

    Describe userid required test assertions

--- services/auths.spec.js
+++ services/auths.spec.js
@@ -15,4 +15,20 @@ describe('/auths', () => {
         });
       });
   });
+
+  it('[POST] /api/auths (userId required)', () => {
+    return request(server)
+      .post('/api/auths')
+      .send({
+        accessToken: 'xxxuser.YHHASY.ANAH72c.77HSY.HSH',
+      })
+      .expect(400)
+      .expect('Content-Type', /json/)
+      .then(res => {
+        expect(res.body).toBeInstanceOf(Object);
+        expect(res.body).toEqual({
+          message: 'Missing required userId field',
+        });
+      });
+  });
 });

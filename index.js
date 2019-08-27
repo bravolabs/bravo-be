0cab170c0fe390b41e38117dd209e82f2ad0eaa8
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-27 14:26:39 +0100

    CHore: Install cors

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const { port } = require('./config');
+const cors = require('cors');
 
 const app = express();
 
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 routes(app);

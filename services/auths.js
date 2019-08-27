73b121a3f1336197cb5b1b22a985750b5569da10
erozonachi <erozonachi.james@gmail.com>
2019-08-27 13:26:35 +0100

    Add and export login-user function to auths module

--- services/auths.js
+++ services/auths.js
@@ -1,5 +1,36 @@
 const users = require('../data/dbModels/users');
+const organizations = require('../data/dbModels/organizations');
+const axios = require('axios');
+const { slack, } = require('../config');
+
+async function loginUser(accessToken, userId) {
+  const user = await users.readBySlackId(userId);
+
+  const res = await axios.get(`${slack.baseUrl}/users.info?token=${accessToken}&user=${userId}`);
+  if(res.user && res.user.id) {
+    if(!user || !user.slack_mem_id || user.slack_mem_id !== res.user.id) {
+      const organization = await organizations.create({
+        slack_org_id: res.user.team_id,
+        name: '...',
+      });
+
+      await users.create({
+        org_id: organization.id,
+        slack_mem_id: res.user.id,
+        name: res.user.name,
+        email: res.user.profile.email,
+      });
+    }
+  }
+  
+  return {
+    statusCode: 401,
+    data: {
+      message: 'Authentication failed.',
+    },
+  }
+}
 
 module.exports = {
-
+  loginUser,
 }

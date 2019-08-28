e3c83280b63513ad6a0b8feea72f517580b5a49d
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-28 04:32:47 +0100

    Chore: Merge back networking concerns and express layer to be on a sngle file, issues coming up tomorrow as a result of this would be solved tomorrow

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const app = require('./api');
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes');
 const { port } = require('./config');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+routes(app);
+
+module.exports = app;
 
 app.listen(port, () => {
   console.log(`running on ${port}`);

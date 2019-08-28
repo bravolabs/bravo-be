573e076d0dad96b72a4aa75b50221aec86e86de7
erozonachi <erozonachi.james@gmail.com>
2019-08-28 08:59:55 +0100

    Require and use helmet in server

--- express-server.js
+++ express-server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 routes(app);

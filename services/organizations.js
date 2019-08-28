21cd00faec09e5a32734d0f38f05e7a9dbf11f55
erozonachi <erozonachi.james@gmail.com>
2019-08-28 15:35:48 +0100

    Add get-shoutouts function to organizations service

--- services/organizations.js
+++ services/organizations.js
@@ -1,3 +1,25 @@
 const organizations = require('../data/dbModels/organizations');
 
-module.exports = {};
+async function getShoutouts(id) {
+  const organization = await organizations.read(id);
+  if (!organization || !organization.slack_org_id) {
+    return {
+      statusCode: 404,
+      data: {
+        message: 'Organization does not exist',
+      },
+    };
+  }
+  const shoutouts = organizations.getShoutouts(organization.id);
+
+  return {
+    statusCode: 200,
+    data: {
+      data: shoutouts,
+    },
+  };
+}
+
+module.exports = {
+  getShoutouts,
+};

26978e884dbce5efc043371b5913f5d9cede63d5
erozonachi <erozonachi.james@gmail.com>
2019-08-28 09:26:36 +0100

    Require and use cors in server file

--- express-server.js
+++ express-server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const helmet = require('helmet');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(helmet());
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 routes(app);

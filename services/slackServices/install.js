b844014e7f827e44caac74c8427c0e1b381d9b56
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-28 03:53:35 +0100

    Fix: Refactor the pattern of how the install service is written

--- services/slackServices/install.js
+++ services/slackServices/install.js
@@ -4,15 +4,20 @@ const { slack } = require('../../config');
 const events = require('events');
 const eventEmitter = new events.EventEmitter();
 
-exports.onBoardUsers = () => {};
+exports.onBoardUsers = async reqInfo => { 
+};
 
-exports.completeInstall = installData => {
-  const organizationDetails = {
-    slack_org_id: installData.orgId,
-    name: installData.orgName,
-    channel_name: installData.channelName,
-    channel_id: installData.channelId,
-    access_token: installData.accessToken,
-  };
-  dbModel.create(organizationDetails);
+exports.completeInstall = async installData => {
+  try {
+    const organizationDetails = {
+      slack_org_id: installData.orgId,
+      name: installData.orgName,
+      channel_name: installData.channelName,
+      channel_id: installData.channelId,
+      access_token: installData.accessToken,
+    };
+    dbModel.create(organizationDetails);
+  } catch (err) {
+    console.log(err);
+  }
 };

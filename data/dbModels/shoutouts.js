0414553313ad04fe0bbdc2adbd01f24ec36d387f
erozonachi <erozonachi.james@gmail.com>
2019-08-29 15:06:09 +0100

    Refactor read function in shoutouts model module

--- data/dbModels/shoutouts.js
+++ data/dbModels/shoutouts.js
@@ -8,8 +8,35 @@ async function create(shoutout) {
   return result[0];
 }
 
-function read(receiver_id) {
-  return db('shoutouts').where({ receiver_id });
+function read(receiver_id = null, id = null) {
+  if (receiver_id) {
+    return db
+      .select(
+        's.id',
+        'message',
+        'created_at',
+        db.ref('g.slack_mem_id').as('giverSlackId'),
+        db.ref('r.slack_mem_id').as('receiverSlackId')
+      )
+      .from(db.ref('shoutouts').as('s'))
+      .join(db.ref('users').as('g'), 's.giver_id', 'g.id')
+      .join(db.ref('users').as('r'), 's.receiver_id', 'r.id')
+      .whereRaw(`s.receiver_id = ${receiver_id}`);
+  }
+
+  return db
+    .select(
+      's.id',
+      'message',
+      'created_at',
+      db.ref('g.slack_mem_id').as('giverSlackId'),
+      db.ref('r.slack_mem_id').as('receiverSlackId')
+    )
+    .from(db.ref('shoutouts').as('s'))
+    .join(db.ref('users').as('g'), 's.giver_id', 'g.id')
+    .join(db.ref('users').as('r'), 's.receiver_id', 'r.id')
+    .whereRaw(`s.id = ${id}`)
+    .first();
 }
 
 module.exports = {

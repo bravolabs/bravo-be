0f0e36315b028f86730aca4ea7a4e34c499e807f
erozonachi <erozonachi.james@gmail.com>
2019-08-29 15:37:00 +0100

    Modify users /get path to point to shoutouts

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ const { validateId } = require('./utils/validator');
 const router = express.Router();
 router.use('/', auth.authenticate);
 
-router.get('/:userId', validateId, async (req, res, next) => {
+router.get('/:userId/shoutouts', validateId, async (req, res, next) => {
   try {
     const { userId } = req.params;
     const result = await service.getShoutouts(userId);

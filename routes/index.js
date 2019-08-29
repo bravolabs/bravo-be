effd1de2390ea10cfef3a8724b1416a513d204e9
erozonachi <erozonachi.james@gmail.com>
2019-08-29 15:35:21 +0100

    Use users route in routes entry file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const shoutouts = require('./shoutouts');
 const auths = require('./auths');
 const organizations = require('./organizations');
+const users = require('./users');
 const commands = require('./commands');
 const interactions = require('./interactions');
 const install = require('./install');
@@ -9,6 +10,7 @@ function routes(app) {
   app.use('/api/shoutouts', shoutouts);
   app.use('/api/auths', auths);
   app.use('/api/organizations', organizations);
+  app.use('/api/users', users);
   app.use('/slack/command', commands);
   app.use('/slack/interactions', interactions);
   app.use('/slack/install', install);

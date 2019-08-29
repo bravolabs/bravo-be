23bcfb6fd63be7440cee301edd368c4f9bfa1e55
erozonachi <erozonachi.james@gmail.com>
2019-08-29 15:30:55 +0100

    Add validate-id function to validator utils module

--- routes/utils/validator.js
+++ routes/utils/validator.js
@@ -1,20 +1,29 @@
 function validateLoginCredentials(req, res, next) {
-  const { accessToken, userId, } = req.body;
-  if(!accessToken || accessToken.trim() === '') {
-    return res.status(400)
-      .json({
-        message: 'Missing required accessToken field'
-      });
+  const { accessToken, userId } = req.body;
+  if (!accessToken || accessToken.trim() === '') {
+    return res.status(400).json({
+      message: 'Missing required accessToken field',
+    });
   }
-  if(!userId || userId.trim() === '') {
-    return res.status(400)
-      .json({
-        message: 'Missing required userId field'
-      });
+  if (!userId || userId.trim() === '') {
+    return res.status(400).json({
+      message: 'Missing required userId field',
+    });
+  }
+  next();
+}
+
+function validateId(req, res, next) {
+  const { userId, id } = req.params;
+  if ((!userId || userId.trim() === '') && (!id || id.trim() === '')) {
+    return res.status(400).json({
+      message: `Missing required ${userId ? 'userId' : 'id'} field`,
+    });
   }
   next();
 }
 
 module.exports = {
   validateLoginCredentials,
-}
+  validateId,
+};

5a8b055ac84dce7eca3de6d8b7860581ddf82c42
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-08-29 03:40:20 +0100

    Chore: Create a new migration for altering the shoutouts table

---
+++ data/migrations/20190829032345_alter-shoutouts.js
@@ -0,0 +1,11 @@
+exports.up = function(knex) {
+  return knex.schema.table('shoutouts', tbl => {
+    tbl.text('organization_id').notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.table('shoutouts', tbl => {
+    tbl.dropColumns('organization_id').notNullable();
+  });
+};

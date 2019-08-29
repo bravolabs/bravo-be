cfb2e5e52abc3cc3b5fdf6bef572018c74decaec
erozonachi <erozonachi.james@gmail.com>
2019-08-29 15:33:34 +0100

    Use validate-id function in users route module

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const service = require('../services/users');
 const auth = require('./utils/auth');
+const { validateId } = require('./utils/validator');
 
 const router = express.Router();
 router.use('/', auth.authenticate);
 
-router.get('/:userId', async (req, res, next) => {
+router.get('/:userId', validateId, async (req, res, next) => {
   try {
     const { userId } = req.params;
     const result = await service.getShoutouts(userId);

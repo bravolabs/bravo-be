74937daaef622659cfcfb612ce31a3ffa2fe0a24
erozonachi <erozonachi.james@gmail.com>
2019-08-29 16:36:54 +0100

    Handle pg error with code: 42703

--- routes/utils/errorHandler.js
+++ routes/utils/errorHandler.js
@@ -1,4 +1,8 @@
 function handleError(error, req, res, next) {
+  if (error.code === '42703') {
+    return res.status(404).json({ message: 'Resource not found' });
+  }
+
   res.status(500).json({
     error: 'server error',
   });

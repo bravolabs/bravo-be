24d18b6af8c037e6ffa4cdaa81f4559e9c544410
erozonachi <erozonachi.james@gmail.com>
2019-08-29 15:49:14 +0100

    Modify get-shoutouts() to get a shoutout by id

--- services/shoutouts.js
+++ services/shoutouts.js
@@ -1,23 +1,12 @@
-const users = require('../data/dbModels/users');
 const shoutouts = require('../data/dbModels/shoutouts');
 
-async function getShoutouts(userId) {
-  const user = await users.readBySlackId(userId);
-  if (!user || !user.slack_mem_id) {
-    return {
-      statusCode: 404,
-      data: {
-        message: 'User not found',
-      },
-    };
-  }
-
-  const result = await shoutouts.read(userId);
+async function getShoutouts(id) {
+  const result = await shoutouts.read(null, id);
   if (result.length < 1) {
     return {
       statusCode: 404,
       data: {
-        message: 'No shoutouts found for the user',
+        message: 'No shoutout found',
       },
     };
   }

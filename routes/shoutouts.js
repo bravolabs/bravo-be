cb87ab79db2e060559c254cf041e0e5046a5df87
erozonachi <erozonachi.james@gmail.com>
2019-08-29 15:43:06 +0100

    Use validateid function in shoutouts route module

--- routes/shoutouts.js
+++ routes/shoutouts.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const service = require('../services/shoutouts');
 const auth = require('./utils/auth');
+const { validateId } = require('./utils/validator');
 
 const router = express.Router();
 router.use('/', auth.authenticate);
 
-router.get('/:id', async (req, res, next) => {
+router.get('/:id', validateId, async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await service.getShoutouts(id);

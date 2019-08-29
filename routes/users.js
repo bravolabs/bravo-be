fb0c17c3ac4719820a13ee710975747e6eedbfa8
erozonachi <erozonachi.james@gmail.com>
2019-08-29 15:19:04 +0100

    Add and export users route module

---
+++ routes/users.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const service = require('../services/users');
+const auth = require('./utils/auth');
+
+const router = express.Router();
+router.use('/', auth.authenticate);
+
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const { userId } = req.params;
+    const result = await service.getShoutouts(userId);
+    res.status(result.statusCode).json(result.data);
+  } catch (error) {
+    res.status(500).json({
+      error: 'server error',
+    });
+  }
+});
+
+module.exports = router;

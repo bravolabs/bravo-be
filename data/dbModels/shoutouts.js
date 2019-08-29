158e64fbd300e8c9ceba81b003d693555c64a14b
erozonachi <erozonachi.james@gmail.com>
2019-08-29 16:23:40 +0100

    Refactor read() to return all shoutouts for a user

--- data/dbModels/shoutouts.js
+++ data/dbModels/shoutouts.js
@@ -8,8 +8,8 @@ async function create(shoutout) {
   return result[0];
 }
 
-function read(receiver_id = null, id = null) {
-  if (receiver_id) {
+function read(userId = null, id = null) {
+  if (userId) {
     return db
       .select(
         's.id',
@@ -21,7 +21,7 @@ function read(receiver_id = null, id = null) {
       .from(db.ref('shoutouts').as('s'))
       .join(db.ref('users').as('g'), 's.giver_id', 'g.id')
       .join(db.ref('users').as('r'), 's.receiver_id', 'r.id')
-      .whereRaw(`s.receiver_id = ${receiver_id}`);
+      .whereRaw(`s.receiver_id = ${userId} OR s.giver_id = ${userId}`);
   }
 
   return db

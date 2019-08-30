231e37d803f196a3876246c3c9061123953a79bf
erozonachi <erozonachi.james@gmail.com>
2019-08-30 14:25:41 +0100

    Require and use validateid() in organization service

--- routes/organizations.js
+++ routes/organizations.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const service = require('../services/organizations');
 const auth = require('./utils/auth');
+const { validateId } = require('./utils/validator');
 
 const router = express.Router();
 router.use('/', auth.authenticate);
-router.get('/:id/shoutouts', async (req, res, next) => {
+router.get('/:id/shoutouts', validateId, async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await service.getShoutouts(id);

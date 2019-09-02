e59f5bd6cfa3278abc7eb5a9bc77e73069dd8d26
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-02 16:48:54 +0100

    Chore: Uncomment the savetoshoutout code

--- services/slackServices/shoutout.helpers.js
+++ services/slackServices/shoutout.helpers.js
@@ -29,13 +29,13 @@ async function getUser(userSlackId, orgId, accessToken) {
 
 exports.saveToDatabase = async dbInfo => {
   const reciever = await getUser(dbInfo.receiver_id, dbInfo.organization_id, dbInfo.access_token);
-  // const giver = await getUser(dbInfo.giver_id, dbInfo.organization_id, dbInfo.access_token);
+  const giver = await getUser(dbInfo.giver_id, dbInfo.organization_id, dbInfo.access_token);
 
-  // const shoutoutData = {
-  //   giver_id: giver.id,
-  //   receiver_id: reciever.id,
-  //   message: dbInfo.message,
-  // };
+  const shoutoutData = {
+    giver_id: giver.id,
+    receiver_id: reciever.id,
+    message: dbInfo.message,
+  };
 
-  // await ShoutOut.create(shoutoutData);
+  await ShoutOut.create(shoutoutData);
 };

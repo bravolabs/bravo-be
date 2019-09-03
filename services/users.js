8d3a5a673a1e6824c3995c0d8c1d10ab11646296
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-03 07:27:48 +0100

    Add org id to organisations read

--- services/users.js
+++ services/users.js
@@ -1,7 +1,36 @@
 const users = require('../data/dbModels/users');
 const shoutouts = require('../data/dbModels/shoutouts');
+const slackUsers = require('./slackServices/users');
 
 async function getShoutouts(userId) {
+  const user = await users.readBySlackId(userId);
+  if (!user || !user.slack_mem_id) {
+    return {
+      statusCode: 404,
+      data: {
+        message: 'User not found',
+      },
+    };
+  }
+
+  const result = await shoutouts.read(user.id);
+  if (result.length < 1) {
+    return {
+      statusCode: 404,
+      data: {
+        message: 'No shoutouts found for the user',
+      },
+    };
+  }
+  return {
+    statusCode: 200,
+    data: {
+      data: result,
+    },
+  };
+}
+
+async function getUserInfo(userId) {
   const user = await users.readBySlackId(userId);
   if (!user || !user.slack_mem_id) {
     return {

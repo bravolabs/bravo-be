d91dcc35c1192452be7daa73aab07f6d52db2c72
Noble Obioma <nobioma1@gmail.com>
2019-09-08 12:29:17 +0100

    refactor login user service to focus on just one task

--- services/auths.js
+++ services/auths.js
@@ -1,49 +1,17 @@
-const axios = require('axios');
 const jwt = require('jsonwebtoken');
-const { secret, slack } = require('../config');
-const organizations = require('../data/dbModels/organizations');
-const users = require('../data/dbModels/users');
+const { secret } = require('../config');
 
-async function loginUser(accessToken, userId) {
-  // get user infomation from slack
-  const { data: slackRes } = await axios.get(
-    `${slack.baseUrl}/users.info?token=${accessToken}&user=${userId}`
-  );
-  if ('user' in slackRes) {
-    const { user: slackUser } = slackRes;
-    // get user's organization
-    const userOrg = await organizations.read(slackUser.team_id);
-    if (userOrg) {
-      // if organization on Db check if user, else create user
-      let user = await users.readBySlackId(userId);
-      if (!user) {
-        user = await users.create({
-          name: slackUser.real_name,
-          slack_mem_id: slackUser.id,
-          org_id: userOrg.id,
-          avatar: slackUser.profile.image_512,
-        });
-      }
-      const token = jwt.sign(user, secret, { expiresIn: '30d' });
-      return {
-        statusCode: 200,
-        data: {
-          name: user.name,
-          avatar: user.avatar,
-          token,
-        },
-      };
-    } else {
-      return {
-        statusCode: 404,
-        data: {
-          message: 'Workspace not found. Please contact your workspace admin to install Bravo',
-        },
-      };
-    }
-  }
-
-  throw new Error(slackRes.error);
+async function loginUser(user) {
+  const token = jwt.sign(user, secret, { expiresIn: '30d' });
+  return {
+    statusCode: 200,
+    data: {
+      id: user.id,
+      name: user.name,
+      avatar: user.avatar,
+      token,
+    },
+  };
 }
 
 module.exports = {

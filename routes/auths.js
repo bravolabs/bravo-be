a9c75a5d9777ea2b9a8ab86b7fa685c437cf177e
Noble Obioma <nobioma1@gmail.com>
2019-09-08 12:30:22 +0100

    Add middlewares to auth routes and pass user object in request object to loginUser service

--- routes/auths.js
+++ routes/auths.js
@@ -1,19 +1,29 @@
 const express = require('express');
 const { validateLoginCredentials } = require('./utils/validator');
+const verifySlackToken = require('./utils/verifySlackToken');
+const organizationExists = require('./utils/organizationExists');
+const userExists = require('./utils/userExists');
 const service = require('../services/auths');
 const shoutout = require('../data/dbModels/shoutouts');
 
 const router = express.Router();
 
-router.post('/', validateLoginCredentials, async (req, res, next) => {
-  try {
-    const { accessToken, userId } = req.body;
-    const result = await service.loginUser(accessToken, userId);
-    res.status(result.statusCode).json(result.data);
-  } catch (error) {
-    next(error);
+router.post(
+  '/',
+  validateLoginCredentials,
+  verifySlackToken,
+  organizationExists,
+  userExists,
+  async (req, res, next) => {
+    const { user } = req;
+    try {
+      const result = await service.loginUser(user);
+      res.status(result.statusCode).json(result.data);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 router.post('/add', async (req, res, next) => {
   try {

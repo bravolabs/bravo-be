7648124a864651e75609d14458fb1937d355bf1b
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-09 13:57:11 +0100

    Chore: Refactor the installation flow into moving parts

--- data/slackComponents/attachments.js
+++ data/slackComponents/attachments.js
@@ -81,3 +81,57 @@ exports.channelNotification = (data, type = null) => {
   ]);
   return attachment;
 };
+
+exports.onboardingAttachments = () => {
+  const attachment = JSON.stringify([
+    {
+      fallback: 'Bravo Onboarding Message',
+      callback: 'installation onboarding',
+      attachment_type: 'default',
+      text:
+        '*How it works?* \n Just type `/bravo` shoutout and I will guide you through the process \n \n  *Our mission:* \n Award your peers with acknowledgments that act like coins/points in Slack when they do awesome things - and never let the acknowledgment of their good work get lost in the shuffle again. \n \n With bravo you will be able to give shoutouts to your team and collegues really easily. Also you will be able to see all the feedback and shoutouts that you get in your dashboard.',
+      color: '#4265ED',
+    },
+  ]);
+  return attachment;
+};
+
+exports.helpOnboardingAttachments = () => {
+  const attachment = JSON.stringify([
+    {
+      fallback: 'Bravo Onboarding Message',
+      callback_id: 'shoutout',
+      attachment_type: 'default',
+      text: 'Give a shoutout',
+      color: '#A9A9A9',
+      divider: true,
+      actions: [
+        {
+          name: 'Send a Shoutout',
+          text: 'Send a Shoutout',
+          type: 'button',
+          value: 'give',
+          style: 'default',
+        },
+      ],
+    },
+    {
+      fallback: 'Bravo Onboarding Message',
+      callback_id: 'shoutout',
+      attachment_type: 'default',
+      text: 'View shoutouts of the selected user',
+      color: '#A9A9A9',
+      divider: true,
+      actions: [
+        {
+          name: 'View shoutouts',
+          text: 'View shoutouts',
+          type: 'button',
+          value: 'retrieve',
+          style: 'default',
+        },
+      ],
+    },
+  ]);
+  return attachment;
+};

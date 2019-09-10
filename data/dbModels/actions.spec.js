82c5d06c065723aaf3c1aedc98e757b2db17ad03
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 13:41:04 +0100

    Copy and modify test staging from shoutouts.spec

--- data/dbModels/actions.spec.js
+++ data/dbModels/actions.spec.js
@@ -1,2 +1,15 @@
 const db = require('../dbConfig');
 const action = require('./actions');
+
+beforeEach(async () => {
+  await db.raw('truncate actions cascade;');
+});
+
+afterEach(async () => {
+  await db.raw('truncate actions cascade;');
+});
+
+afterAll(async done => {
+  await new Promise(resolve => setTimeout(() => resolve(), 500));
+  done();
+});

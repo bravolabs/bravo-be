8e7e3a41aab84f16b6c12208bdd36ccef9fc378d
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 15:32:15 +0100

    Add read for basic transaction data

--- data/dbModels/transactions.js
+++ data/dbModels/transactions.js
@@ -12,11 +12,32 @@ async function create(transaction) {
   }
 }
 
+async function read(orgId, offset = 0, limit = 50) {
+  try {
+    return db
+      .select(
+        db.ref('t.id').as('id'),
+        'created_at',
+        db.ref('t.org_id').as('orgId'),
+        db.ref('a.reward').as('reward'),
+        db.ref('a.name').as('action')
+      )
+      .from(db.ref('transactions').as('t'))
+      .join(db.ref('actions').as('a'), 't.action_id', 'a.id')
+      .whereRaw(`t.org_id = ${orgId}`)
+      .orderBy('created_at', 'desc')
+      .limit(limit)
+      .offset(offset);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
 async function readWithData(orgId, offset = 0, limit = 20) {
   try {
     return db
       .select(
-        't.id',
+        db.ref('t.id').as('id'),
         'created_at',
         db.ref('o.id').as('orgId'),
         db.ref('g.slack_mem_id').as('giverSlackId'),
@@ -29,11 +50,11 @@ async function readWithData(orgId, offset = 0, limit = 20) {
         db.ref('a.name').as('action')
       )
       .from(db.ref('transactions').as('t'))
-      .join(db.ref('users').as('g'), 's.giver_id', 'g.id')
-      .join(db.ref('users').as('r'), 's.receiver_id', 'r.id')
-      .join(db.ref('organisations').as('o'), 's.org_id', 'o.id')
-      .join(db.ref('actions').as('a'), 's.action_id', 'a.id')
-      .whereRaw(`s.org_id = ${orgId}`)
+      .join(db.ref('users').as('g'), 't.giver_id', 'g.id')
+      .join(db.ref('users').as('r'), 't.receiver_id', 'r.id')
+      .join(db.ref('organisations').as('o'), 't.org_id', 'o.id')
+      .join(db.ref('actions').as('a'), 't.action_id', 'a.id')
+      .whereRaw(`t.org_id = ${orgId}`)
       .orderBy('created_at', 'desc')
       .limit(limit)
       .offset(offset);

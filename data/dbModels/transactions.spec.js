4c3f502e99e3768548e898c34cf5be66575349bd
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 13:33:34 +0100

    Add imports for transactions spec file

---
+++ data/dbModels/transactions.spec.js
@@ -0,0 +1,5 @@
+const db = require('../dbConfig');
+const shoutout = require('./shoutouts');
+const user = require('./users');
+const orgs = require('./organizations');
+const transaction = require('./transactions');

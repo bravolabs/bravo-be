b3a95dcf9f3744a78899bf9e16bcf509c7754855
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 20:46:56 +0100

    Create transaction in ProcessTransaction

--- services/wallet.js
+++ services/wallet.js
@@ -1,7 +1,24 @@
 const transactions = require('../data/dbModels/transactions');
+const actions = require('../data/dbModels/actions');
 const wallets = require('../data/dbModels/wallets');
 
-async function ProcessTransaction(userId, giverId, orgId, shoutoutId, action) {}
+async function ProcessTransaction(userId, giverId, orgId, shoutoutId, actionNameOrId) {
+  let action;
+  if (typeof actionNameOrId === 'string') {
+    action = await actions.readByName(actionNameOrId);
+  } else {
+    action = await actions.readById(actionNameOrId);
+  }
+  if (!action) throw new Error('action invalid');
+
+  const transaction = await transactions.create({
+    org_id: orgId,
+    giver_id: giverId,
+    receiver_id: userId,
+    action_id: action.id,
+    shoutout_id: shoutoutId,
+  });
+}
 
 module.exports = {
   ProcessTransaction,

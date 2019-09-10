71766168847961c55dc6b18e0689360f7d110c50
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 14:43:50 +0100

    Add create transaction function

--- data/dbModels/transactions.js
+++ data/dbModels/transactions.js
@@ -1,2 +1,13 @@
 const db = require('../dbConfig');
 const util = require('util');
+
+async function create(transaction) {
+  try {
+    const result = await db('transactions')
+      .insert(transaction)
+      .returning('*');
+    return result[0];
+  } catch (err) {
+    console.log(err);
+  }
+}

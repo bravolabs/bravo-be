59dbf0c13deca668177843a2de7d1ef27e588f6c
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 13:34:58 +0100

    Copy and modify test staging from shoutouts.spec

--- data/dbModels/transactions.spec.js
+++ data/dbModels/transactions.spec.js
@@ -3,3 +3,18 @@ const shoutout = require('./shoutouts');
 const user = require('./users');
 const orgs = require('./organizations');
 const transaction = require('./transactions');
+
+beforeEach(async () => {
+  await db.raw('truncate organizations cascade;');
+  await db.raw('truncate transactions cascade;');
+});
+
+afterEach(async () => {
+  await db.raw('truncate organizations cascade;');
+  await db.raw('truncate transactions cascade;');
+});
+
+afterAll(async done => {
+  await new Promise(resolve => setTimeout(() => resolve(), 500));
+  done();
+});

9d9280b48c6de0ed7a03e1afc83b0a0fb6f7cb5e
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 18:41:01 +0100

    Add pagination to service functions

--- services/transactions.js
+++ services/transactions.js
@@ -3,8 +3,14 @@ const organizations = require('../data/dbModels/organizations');
 
 const pageLimit = Number(process.env.TRANSACTION_PAGE_LIMIT || '50');
 
-async function getTransactionsForOrganization(orgId) {
-  const result = await transactions.read(orgId);
+const clamp = (num, min, max) => {
+  return Math.min(Math.max(num, min), max);
+};
+
+async function getTransactionsForOrganization(orgId, page = 1, pageSize = pageLimit) {
+  let size = clamp(pageSize, 1, pageLimit);
+  const offset = (page - 1) * size;
+  const result = await transactions.read(orgId, offset, size);
   if (!result) {
     return {
       statusCode: 404,
@@ -21,8 +27,10 @@ async function getTransactionsForOrganization(orgId) {
   };
 }
 
-async function getFullTransactionsForOrganization(orgId) {
-  const result = await transactions.readWithData(orgId);
+async function getFullTransactionsForOrganization(orgId, page = 1, pageSize = pageLimit) {
+  let size = clamp(pageSize, 1, pageLimit);
+  const offset = (page - 1) * size;
+  const result = await transactions.readWithData(orgId, offset, size);
   if (!result) {
     return {
       statusCode: 404,

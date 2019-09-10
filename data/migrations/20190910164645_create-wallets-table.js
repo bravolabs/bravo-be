59f82cbe981ba9bea8c4f015f751e0cf3aef8310
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 16:50:09 +0100

    Create wallets table migration

---
+++ data/migrations/20190910164645_create-wallets-table.js
@@ -0,0 +1,20 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('wallets', tbl => {
+    tbl.increments();
+    tbl
+      .integer('user_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl
+      .integer('amount')
+      .notNullable()
+      .defaultTo(0);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('wallets');
+};

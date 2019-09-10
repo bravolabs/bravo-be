eaa2bb5df4574f0b02f7ba141c1668c282b15dc3
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 17:45:10 +0100

    Add create wallet function to wallets model

--- data/dbModels/wallets.js
+++ data/dbModels/wallets.js
@@ -1,1 +1,16 @@
 const db = require('../dbConfig');
+
+async function create(wallet) {
+  try {
+    const result = await db('wallets')
+      .insert(wallet)
+      .returning('*');
+    return result[0];
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+module.exports = {
+  create,
+};

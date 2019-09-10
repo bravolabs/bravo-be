33d6b4d87e3d19a5465a5677f8d38899b26e46cd
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 17:23:26 +0100

    Add test staging functions for wallets

--- data/dbModels/wallets.spec.js
+++ data/dbModels/wallets.spec.js
@@ -1,2 +1,27 @@
 const db = require('../dbConfig');
 const walletsModel = require('./wallets');
+const usersModel = require('./users');
+
+beforeEach(async () => {
+  await db.raw('truncate wallets cascade;');
+  await db.raw('truncate users cascade;');
+});
+
+afterEach(async () => {
+  await db.raw('truncate wallets cascade;');
+  await db.raw('truncate users cascade;');
+});
+
+afterAll(async done => {
+  await new Promise(resolve => setTimeout(() => resolve(), 500));
+  done();
+});
+
+const stageData = async () => {
+  return usersModel.create({
+    org_id: org_id,
+    slack_mem_id: 'testing123',
+    email: 'iamatest@gmail.com',
+    name: 'test',
+  });
+};

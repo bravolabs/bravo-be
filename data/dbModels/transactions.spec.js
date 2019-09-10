bc44c5e23d1c03e6b0cb67f7f68c4e75473d8b64
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 14:34:45 +0100

    Add stageData function for transaction tests

--- data/dbModels/transactions.spec.js
+++ data/dbModels/transactions.spec.js
@@ -18,3 +18,40 @@ afterAll(async done => {
   await new Promise(resolve => setTimeout(() => resolve(), 500));
   done();
 });
+
+const stageData = async () => {
+  const { id: org_id } = await orgs.create({
+    slack_org_id: 'dhbsh3ii3',
+    name: 'hbbwej3',
+    channel_name: 'bots',
+    channel_id: 'ejwjkqe',
+    access_token: 'bfqhjwfbwjf',
+  });
+
+  const { id: giver_id } = await user.create({
+    org_id: org_id,
+    slack_mem_id: '773jjf',
+    email: 'ogwurujohnson@gmail.com',
+    name: 'Johnny',
+  });
+
+  const { id: receiver_id } = await user.create({
+    org_id: org_id,
+    slack_mem_id: '773jjfaf',
+    email: 'aaronthompson@gmail.com',
+    name: 'Aaron',
+  });
+
+  const { id: shoutout_id } = await shoutout.create({
+    giver_id,
+    receiver_id,
+    message: 'Amazing Job this week',
+  });
+
+  return {
+    org_id,
+    giver_id,
+    receiver_id,
+    shoutout_id,
+  };
+};

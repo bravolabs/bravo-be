a3775043bf2aa75ac45f2437e8f4708517280b2d
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 15:37:38 +0100

    Pull full join for transactions to a seperate function

--- data/dbModels/transactions.js
+++ data/dbModels/transactions.js
@@ -12,6 +12,28 @@ async function create(transaction) {
   }
 }
 
+const transactionsFullyJoined = () => {
+  return db
+    .select(
+      db.ref('t.id').as('id'),
+      'created_at',
+      db.ref('o.id').as('orgId'),
+      db.ref('g.slack_mem_id').as('giverSlackId'),
+      db.ref('g.name').as('giverName'),
+      db.ref('g.avatar').as('giverAvatar'),
+      db.ref('r.slack_mem_id').as('receiverSlackId'),
+      db.ref('r.name').as('receiverName'),
+      db.ref('r.avatar').as('receiverAvatar'),
+      db.ref('a.reward').as('reward'),
+      db.ref('a.name').as('action')
+    )
+    .from(db.ref('transactions').as('t'))
+    .join(db.ref('users').as('g'), 't.giver_id', 'g.id')
+    .join(db.ref('users').as('r'), 't.receiver_id', 'r.id')
+    .join(db.ref('organisations').as('o'), 't.org_id', 'o.id')
+    .join(db.ref('actions').as('a'), 't.action_id', 'a.id');
+};
+
 async function read(orgId, offset = 0, limit = 50) {
   try {
     return db
@@ -35,25 +57,7 @@ async function read(orgId, offset = 0, limit = 50) {
 
 async function readByUser(userId, offset = 0, limit = 20) {
   try {
-    return db
-      .select(
-        db.ref('t.id').as('id'),
-        'created_at',
-        db.ref('o.id').as('orgId'),
-        db.ref('g.slack_mem_id').as('giverSlackId'),
-        db.ref('g.name').as('giverName'),
-        db.ref('g.avatar').as('giverAvatar'),
-        db.ref('r.slack_mem_id').as('receiverSlackId'),
-        db.ref('r.name').as('receiverName'),
-        db.ref('r.avatar').as('receiverAvatar'),
-        db.ref('a.reward').as('reward'),
-        db.ref('a.name').as('action')
-      )
-      .from(db.ref('transactions').as('t'))
-      .join(db.ref('users').as('g'), 't.giver_id', 'g.id')
-      .join(db.ref('users').as('r'), 't.receiver_id', 'r.id')
-      .join(db.ref('organisations').as('o'), 't.org_id', 'o.id')
-      .join(db.ref('actions').as('a'), 't.action_id', 'a.id')
+    return transactionsFullyJoined()
       .whereRaw(`t.receiver_id = ${userId} OR t.giver_id = ${userId}`)
       .orderBy('created_at', 'desc')
       .limit(limit)
@@ -65,25 +69,7 @@ async function readByUser(userId, offset = 0, limit = 20) {
 
 async function readWithData(orgId, offset = 0, limit = 20) {
   try {
-    return db
-      .select(
-        db.ref('t.id').as('id'),
-        'created_at',
-        db.ref('o.id').as('orgId'),
-        db.ref('g.slack_mem_id').as('giverSlackId'),
-        db.ref('g.name').as('giverName'),
-        db.ref('g.avatar').as('giverAvatar'),
-        db.ref('r.slack_mem_id').as('receiverSlackId'),
-        db.ref('r.name').as('receiverName'),
-        db.ref('r.avatar').as('receiverAvatar'),
-        db.ref('a.reward').as('reward'),
-        db.ref('a.name').as('action')
-      )
-      .from(db.ref('transactions').as('t'))
-      .join(db.ref('users').as('g'), 't.giver_id', 'g.id')
-      .join(db.ref('users').as('r'), 't.receiver_id', 'r.id')
-      .join(db.ref('organisations').as('o'), 't.org_id', 'o.id')
-      .join(db.ref('actions').as('a'), 't.action_id', 'a.id')
+    return transactionsFullyJoined()
       .whereRaw(`t.org_id = ${orgId}`)
       .orderBy('created_at', 'desc')
       .limit(limit)

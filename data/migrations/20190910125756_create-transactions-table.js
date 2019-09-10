165c337edb425f02f8083ac663eece4f7bcc9240
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 13:14:26 +0100

    Start transactions table. Not finished

--- data/migrations/20190910125756_create-transactions-table.js
+++ data/migrations/20190910125756_create-transactions-table.js
@@ -1,8 +1,30 @@
-
 exports.up = function(knex) {
-  
+  return knex.schema.createTable('transactions', tbl => {
+    tbl.increments();
+    tbl
+      .integer('org_id')
+      .notNullable()
+      .references('id')
+      .inTable('organizations')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl
+      .integer('giver_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+    tbl
+      .integer('receiver_id')
+      .notNullable()
+      .references('id')
+      .inTable('users')
+      .onUpdate('CASCADE')
+      .onDelete('CASCADE');
+  });
 };
 
 exports.down = function(knex) {
-  
+  return knex.schema.dropTableIfExists('transactions');
 };

a12aecf69778d7c013dd8ea790553a5f289f496a
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 13:57:25 +0100

    Add skeletion functions for actions

--- data/dbModels/actions.js
+++ data/dbModels/actions.js
@@ -1,2 +1,16 @@
 const db = require('../dbConfig');
 const util = require('util');
+
+async function create(action) {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function readById(id) {
+  try {
+  } catch (err) {
+    console.log(err);
+  }
+}

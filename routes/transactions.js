d7a6688d53fc3778b8d1b9a2bdd97a914a425a79
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 18:57:24 +0100

    Add route for getting full transactions of organization

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,3 +1,16 @@
 const express = require('express');
 const service = require('../services/transactions');
 const auth = require('./utils/auth');
+
+const router = express.Router();
+router.use('/', auth.authenticate);
+
+router.get('/org/full/:orgId(/:page)?(/:pageSize)?', async (req, res, next) => {
+  try {
+    let { orgId, page, pageSize } = req.params;
+    const result = await service.getFullTransactionsForOrganization(orgId, page, pageSize);
+    res.status(result.statusCode).json(result.data);
+  } catch (error) {
+    next(error);
+  }
+});

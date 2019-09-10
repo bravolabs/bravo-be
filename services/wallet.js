a06e9d9bb14978e099f5db67f79033ea3015c52d
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 20:40:26 +0100

    Create and export ProcessTransaction service function

--- services/wallet.js
+++ services/wallet.js
@@ -1,2 +1,8 @@
 const transactions = require('../data/dbModels/transactions');
 const wallets = require('../data/dbModels/wallets');
+
+async function ProcessTransaction(userId, giverId, orgId, shoutoutId, action) {}
+
+module.exports = {
+  ProcessTransaction,
+};

a52ddeddb2da962281747706d6e84211240fd079
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 20:55:29 +0100

    Catch error, adjust wallet before saving transaction to prevent saving transactions that didn't work

--- services/wallet.js
+++ services/wallet.js
@@ -3,34 +3,38 @@ const actions = require('../data/dbModels/actions');
 const wallets = require('../data/dbModels/wallets');
 
 async function ProcessTransaction(userId, giverId, orgId, shoutoutId, actionNameOrId) {
-  let action;
-  if (typeof actionNameOrId === 'string') {
-    action = await actions.readByName(actionNameOrId);
-  } else {
-    action = await actions.readById(actionNameOrId);
+  try {
+    let action;
+    if (typeof actionNameOrId === 'string') {
+      action = await actions.readByName(actionNameOrId);
+    } else {
+      action = await actions.readById(actionNameOrId);
+    }
+    if (!action) throw new Error('action invalid');
+
+    const userWallet = await wallets.readByUserId(userId);
+    if (!userWallet) throw new Error("Couldn't get user wallet");
+
+    const newAmount = userWallet.amount + action.reward;
+    const updatedWallet = await wallets.updateByUserId(userId, newAmount);
+
+    const transaction = await transactions.create({
+      org_id: orgId,
+      giver_id: giverId,
+      receiver_id: userId,
+      action_id: action.id,
+      shoutout_id: shoutoutId,
+    });
+
+    if (!transaction) throw new Error("transaction couldn't be created");
+
+    return {
+      transaction,
+      wallet: updatedWallet,
+    };
+  } catch (error) {
+    console.log(error);
   }
-  if (!action) throw new Error('action invalid');
-
-  const transaction = await transactions.create({
-    org_id: orgId,
-    giver_id: giverId,
-    receiver_id: userId,
-    action_id: action.id,
-    shoutout_id: shoutoutId,
-  });
-
-  if (!transaction) throw new Error("transaction couldn't be created");
-
-  const userWallet = await wallets.readByUserId(userId);
-  if (!userWallet) throw new Error("Couldn't get user wallet");
-
-  const newAmount = userWallet.amount + action.reward;
-  const updatedWallet = await wallets.updateByUserId(userId, newAmount);
-
-  return {
-    transaction,
-    wallet: updatedWallet,
-  };
 }
 
 module.exports = {

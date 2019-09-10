757c345418e28a262d9e26b964fa107f013c8561
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 18:21:53 +0100

    Create and export getTransactionsByOrganization service function

--- services/transactions.js
+++ services/transactions.js
@@ -1,2 +1,8 @@
 const transactions = require('../data/dbModels/transactions');
 const organizations = require('../data/dbModels/organizations');
+
+async function getTransactionsForOrganization(orgId) {}
+
+module.exports = {
+  getTransactionsForOrganization,
+};

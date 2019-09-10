19742f6939c6e4064e600d611f892ee7b6034fce
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-10 18:24:32 +0100

    Add implementation for afformentioned service function

--- services/transactions.js
+++ services/transactions.js
@@ -1,7 +1,23 @@
 const transactions = require('../data/dbModels/transactions');
 const organizations = require('../data/dbModels/organizations');
 
-async function getTransactionsForOrganization(orgId) {}
+async function getTransactionsForOrganization(orgId) {
+  const result = await transactions.read(orgId);
+  if (!result) {
+    return {
+      statusCode: 404,
+      data: {
+        message: 'No transactions found for organization',
+      },
+    };
+  }
+  return {
+    statusCode: 200,
+    data: {
+      data: result,
+    },
+  };
+}
 
 module.exports = {
   getTransactionsForOrganization,

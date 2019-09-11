d56eabf18aae610aad104aa0f2037942ca3af7b6
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-11 12:47:33 +0100

    Init router with authentication

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,3 +1,6 @@
 const express = require('express');
 const service = require('../services/wallet');
 const auth = require('./utils/auth');
+
+const router = express.Router();
+router.use('/', auth.authenticate);

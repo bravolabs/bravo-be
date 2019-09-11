7bea37d2fac65dcd6b0dbd839591afb582b22cb7
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-11 13:10:26 +0100

    Add beforeAll to stub authentication

--- routes/__tests__/leaderboards.spec.js
+++ routes/__tests__/leaderboards.spec.js
@@ -5,3 +5,18 @@ const user = require('../../data/dbModels/users');
 const orgs = require('../../data/dbModels/organizations');
 const wallets = require('../../data/dbModels/wallets');
 const auth = require('../utils/auth');
+
+let server;
+
+beforeAll(async () => {
+  // We're stubbing the authenticate middleware here. Authenticating with slack seems
+  // to sparingly work in tests, so instead we are just negating it entierly.
+  sinon.stub(auth, 'authenticate').callsFake(function(req, res, next) {
+    // Return a user to ensure organization can be recovered for endpoint
+    req.user = {
+      org_id: 1,
+    };
+    return next();
+  });
+  server = require('../../express-server');
+});

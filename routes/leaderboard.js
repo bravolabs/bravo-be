62608d8141fce03854c435462eaf45a16a06ea87
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-11 12:50:38 +0100

    Add learboard route

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -4,3 +4,14 @@ const auth = require('./utils/auth');
 
 const router = express.Router();
 router.use('/', auth.authenticate);
+
+router.get('/:page?/:pageSize?', async (req, res, next) => {
+  try {
+    let { page, pageSize } = req.params;
+    let orgId = req.user.org_id;
+    const result = await service.getLeaderboardForOrganization(orgId, page, pageSize);
+    res.status(result.statusCode).json(result.data);
+  } catch (error) {
+    next(error);
+  }
+});

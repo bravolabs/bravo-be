47a6de6baa822c1daac9eb2a374984c4732936a3
AaronJThompson <aaronjthompson98@gmail.com>
2019-09-11 13:13:39 +0100

    Add staging data function

--- routes/__tests__/leaderboards.spec.js
+++ routes/__tests__/leaderboards.spec.js
@@ -7,6 +7,7 @@ const wallets = require('../../data/dbModels/wallets');
 const auth = require('../utils/auth');
 
 let server;
+let org_id;
 
 beforeAll(async () => {
   // We're stubbing the authenticate middleware here. Authenticating with slack seems
@@ -14,9 +15,49 @@ beforeAll(async () => {
   sinon.stub(auth, 'authenticate').callsFake(function(req, res, next) {
     // Return a user to ensure organization can be recovered for endpoint
     req.user = {
-      org_id: 1,
+      org_id: org_id || 1,
     };
     return next();
   });
   server = require('../../express-server');
 });
+
+const stageData = async () => {
+  const { id: org_id } = await orgs.create({
+    slack_org_id: 'dhbsh3ii3',
+    name: 'hbbwej3',
+    channel_name: 'bots',
+    channel_id: 'ejwjkqe',
+    access_token: 'bfqhjwfbwjf',
+  });
+
+  const { id: user_1 } = await user.create({
+    org_id: org_id,
+    slack_mem_id: '773jjf',
+    email: 'ogwurujohnson@gmail.com',
+    name: 'Johnny',
+  });
+
+  const { id: user_2 } = await user.create({
+    org_id: org_id,
+    slack_mem_id: '773jjfaf',
+    email: 'aaronthompson@gmail.com',
+    name: 'Aaron',
+  });
+
+  await wallets.create({
+    user_id: user_1,
+    amount: 50,
+  });
+
+  await wallets.create({
+    user_id: user_2,
+    amount: 100,
+  });
+
+  return {
+    org_id,
+    user_1,
+    user_2,
+  };
+};

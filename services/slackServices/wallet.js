b85d3ec9655c39373e5efa2126a2713bd88ed277
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-11 22:17:27 +0100

    Chore: Have the getuserwalletbalance method return an error if the userslackid is not sent

--- services/slackServices/wallet.js
+++ services/slackServices/wallet.js
@@ -2,6 +2,10 @@ const User = require('../../data/dbModels/users');
 const { slackModel } = require('../../data/slackModels/slack');
 const { getUserWallet } = require('../wallet');
 
-exports.getUserWalletBalance = (slackUserId) => {
-  
-}
+exports.getUserWalletBalance = async slackUserId => {
+  try {
+    if (!slackUserId) throw new Error('provide slack userid please');
+  } catch (err) {
+    console.log(err);
+  }
+};

c024fca019abfd3aca0cac573eca355a08ecd8e8
ropeks <ropeksmilioner@gmail.com>
2019-09-13 15:17:30 +0200

    add transaction for reaction

--- routes/events.js
+++ routes/events.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const installService = require('../services/slackServices/install');
+const transaction = require('../services/wallet');
+const User = require('../data/dbModels/users');
+const Organization = require('../data/dbModels/organizations');
+const Shoutout = require('../data/dbModels/shoutouts');
 
 const router = express.Router();
 
@@ -11,6 +15,19 @@ router.post('/', async (req, res) => {
       res.status(200).send('');
       const { user, team } = req.body.event;
       await installService.onboardNewUser(user, team);
+    } else if (req.body.event.type === 'reaction_added' && !req.body.event.item_user) {
+      res.status(200).send('');
+      const reactingUser = await User.readBySlackId(req.body.event.user);
+      const organization = await Organization.read(req.body.team_id);
+      const shoutout = await Shoutout.readByTimestamp(req.body.event.item.ts);
+      const result = await transaction.ProcessTransaction(
+        reactingUser.id,
+        shoutout.giver_id,
+        organization.id,
+        shoutout.id,
+        'reaction'
+      );
+      console.log(result);
     }
   } catch (err) {
     console.log(err);

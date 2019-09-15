8ab9bba0828d528017b26040348643648c5643fa
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-15 15:44:37 +0100

    Chore: Separate the network layer from the srvice layer

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const installService = require('../services/slackServices/install');
-const transaction = require('../services/wallet');
-const User = require('../data/dbModels/users');
-const Organization = require('../data/dbModels/organizations');
-const Shoutout = require('../data/dbModels/shoutouts');
+const transactionService = require('../services/slackServices/transaction');
 
 const router = express.Router();
 
@@ -17,17 +14,12 @@ router.post('/', async (req, res) => {
       await installService.onboardNewUser(user, team);
     } else if (req.body.event.type === 'reaction_added' && !req.body.event.item_user) {
       res.status(200).send('');
-      const reactingUser = await User.readBySlackId(req.body.event.user);
-      const organization = await Organization.read(req.body.team_id);
-      const shoutout = await Shoutout.readByTimestamp(req.body.event.item.ts);
-      const result = await transaction.ProcessTransaction(
-        reactingUser.id,
-        shoutout.giver_id,
-        organization.id,
-        shoutout.id,
-        'reaction'
-      );
-      console.log(result);
+      const reqInfo = {
+        user: req.body.event.user,
+        org_id: req.body.team_id,
+        ts: req.body.event.item.ts,
+      };
+      await transactionService.giveReactionPoint(reqInfo);
     }
   } catch (err) {
     console.log(err);

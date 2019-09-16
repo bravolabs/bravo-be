ee74a6f3b1df1a46e712d1063fd14eca8972caa4
ropeks <ropeksmilioner@gmail.com>
2019-09-16 13:17:05 +0200

    switch reacting user with receiver user id

--- services/slackServices/transaction.js
+++ services/slackServices/transaction.js
@@ -1,15 +1,13 @@
 const transaction = require('../wallet');
-const User = require('../../data/dbModels/users');
 const Organization = require('../../data/dbModels/organizations');
 const Shoutout = require('../../data/dbModels/shoutouts');
 
 exports.givePoint = async reqInfo => {
   try {
-    const reactingUser = await User.readBySlackId(reqInfo.user);
     const organization = await Organization.read(reqInfo.org_id);
     const shoutout = await Shoutout.readByTimestamp(reqInfo.ts);
     await transaction.ProcessTransaction(
-      reactingUser.id,
+      shoutout.receiver_id,
       shoutout.giver_id,
       organization.id,
       shoutout.id,

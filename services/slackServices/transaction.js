c3c14e74961482fb98adfc127c2aa65e33abb446
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-16 09:49:44 +0100

    Chore: Refactor all moving parts

--- services/slackServices/transaction.js
+++ services/slackServices/transaction.js
@@ -3,20 +3,19 @@ const User = require('../../data/dbModels/users');
 const Organization = require('../../data/dbModels/organizations');
 const Shoutout = require('../../data/dbModels/shoutouts');
 
-exports.giveReactionPoint = async reqInfo => {
+exports.givePoint = async reqInfo => {
   try {
     const reactingUser = await User.readBySlackId(reqInfo.user);
     const organization = await Organization.read(reqInfo.org_id);
     const shoutout = await Shoutout.readByTimestamp(reqInfo.ts);
-    const result = await transaction.ProcessTransaction(
+    await transaction.ProcessTransaction(
       reactingUser.id,
       shoutout.giver_id,
       organization.id,
       shoutout.id,
-      'reaction',
-      reqInfo.add_reaction
+      reqInfo.action,
+      reqInfo.fund
     );
-    console.log(result);
     // send a message to the user tellig them what the total amount of coins they have is and that th received 1coin
   } catch (err) {
     console.log(err);

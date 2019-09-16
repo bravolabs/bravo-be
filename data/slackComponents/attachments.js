53a1c9e4a30c8bbbcc571cfc2622976678700848
Johnson Ogwuru <ogwurujohnson@gmail.com>
2019-09-16 09:52:25 +0100

    Chore: change url of single shoutout

--- data/slackComponents/attachments.js
+++ data/slackComponents/attachments.js
@@ -73,7 +73,7 @@ exports.channelNotification = (data, type = null) => {
         {
           type: 'button',
           text: 'View',
-          url: `${data.clientUrl}/shoutout/${data.id}`,
+          url: `${data.clientUrl}/shoutouts/${data.id}`,
         },
       ],
       footer: data.footer,

fa48290af53694c05788fdfc734fc7b36223568e
erozonachi <erozonachi.james@gmail.com>
2019-09-16 11:22:55 +0100

    Update organizations read() call in routes utils

--- routes/utils/organizationExists.js
+++ routes/utils/organizationExists.js
@@ -2,7 +2,7 @@ const organizations = require('../../data/dbModels/organizations');
 
 module.exports = async function organizationExists(req, res, next) {
   const { team: slackWorkspace } = req.slackRes;
-  const userOrg = await organizations.read(slackWorkspace.id);
+  const userOrg = await organizations.read(null, slackWorkspace.id);
   if (!userOrg) {
     return res.status(404).json({
       message: 'Workspace not found. Please contact your workspace admin to install Bravo',

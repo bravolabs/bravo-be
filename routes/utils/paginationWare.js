499848f3051eb7079059baa31d6bbcf9c7b598bb
erozonachi <erozonachi.james@gmail.com>
2019-09-18 20:03:46 +0100

    Add pagination logic to the pagination middleware

--- routes/utils/paginationWare.js
+++ routes/utils/paginationWare.js
@@ -1,3 +1,11 @@
-function paginate(req, res, next) {}
+function paginate(req, res, next) {
+  const { count, page } = req.query;
+  req.limit = (count && parseInt(count)) || 50;
+  req.offset = (count && page && parseInt(count) * parseInt(page)) || 0;
+  req.previous = page && parseInt(page) - 1;
+  req.next = (page && parseInt(page) + 1) || 2;
+
+  next();
+}
 
 module.exports = paginate;

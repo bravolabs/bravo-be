151b48933d1e7f7e63334b1173481cd08a369a6c
erozonachi <erozonachi.james@gmail.com>
2019-09-18 20:11:38 +0100

    Pass paginate info as argument to get-shoutouts()

--- routes/organizations.js
+++ routes/organizations.js
@@ -9,7 +9,8 @@ router.use('/', auth.authenticate);
 router.get('/:id/shoutouts', validateId, paginate, async (req, res, next) => {
   try {
     const { id } = req.params;
-    const result = await service.getShoutouts(id);
+    const { limit, offset, previous, next } = req;
+    const result = await service.getShoutouts(id, { limit, offset, previous, next });
     res.status(result.statusCode).json(result.data);
   } catch (error) {
     next(error);

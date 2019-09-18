4ffd35c26abb2dec0c7782d75bdb50b225ebd58d
erozonachi <erozonachi.james@gmail.com>
2019-09-18 20:08:17 +0100

    Require and use paginate in organizations routes

--- routes/organizations.js
+++ routes/organizations.js
@@ -2,10 +2,11 @@ const express = require('express');
 const service = require('../services/organizations');
 const auth = require('./utils/auth');
 const { validateId } = require('./utils/validator');
+const paginate = require('./utils/paginationWare');
 
 const router = express.Router();
 router.use('/', auth.authenticate);
-router.get('/:id/shoutouts', validateId, async (req, res, next) => {
+router.get('/:id/shoutouts', validateId, paginate, async (req, res, next) => {
   try {
     const { id } = req.params;
     const result = await service.getShoutouts(id);

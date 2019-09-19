a91a0f145a90a2e712545008dc893cd8ee9c2ff5
ropeks <ropeksmilioner@gmail.com>
2019-09-19 16:54:02 +0200

    add leaderboard slash command

--- routes/commands.js
+++ routes/commands.js
@@ -3,13 +3,15 @@ const { slack } = require('../config');
 const shoutOutService = require('../services/slackServices/shoutout');
 const installService = require('../services/slackServices/install');
 const walletService = require('../services/slackServices/wallet');
+const User = require('../data/dbModels/users');
+const Organization = require('../data/dbModels/organizations');
+const leaderboard = require('../services/wallet');
 
 const router = express.Router();
 
 router.post('/', async (req, res) => {
   try {
     const keyword = req.body.text;
-
     const { channel_id, user_id, team_id } = req.body;
     switch (keyword) {
       case 'shoutout':
@@ -30,6 +32,17 @@ router.post('/', async (req, res) => {
           team_id,
         };
         await walletService.getUserWalletBalance(walletReqinfo);
+        break;
+      case 'leaderboard':
+        await res.status(200).send('');
+        const user = await User.readBySlackId(user_id);
+        const org = await Organization.read(null, user.org_id);
+        const data = {
+          channel_id,
+          user_id,
+          access_token: org.access_token,
+        };
+        await leaderboard.getLeaderboardForOrganization(org.id, 1, 50, data);
         break;
       case 'help':
         await res.status(200).send('');

38368c5b6f2bead07dd1bc6e7cb50cd29084f981
ogwurujohnson <ogwurujohnson@gmail.com>
2019-09-21 15:05:17 +0100

    Chore: Setup middleware function for verifying slack token

---
+++ routes/utils/signVerification.js
@@ -0,0 +1,11 @@
+const crypto = require('crypto');
+const qs = require('qs');
+const { slack } = require('../../config');
+
+let verifySignSecret = (req, res, next) => {
+    
+}
+
+module.exports = {
+    verifySignSecret,
+}

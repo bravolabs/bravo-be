3b39355a9b5d85b8a0e229a6c8d51d0501d1593e
ogwurujohnson <ogwurujohnson@gmail.com>
2019-09-21 15:07:07 +0100

    Chore: Retrieve request body

--- routes/utils/signVerification.js
+++ routes/utils/signVerification.js
@@ -5,6 +5,7 @@ const { slack } = require('../../config');
 let verifySignSecret = (req, res, next) => {
     const slackSigningSecret = slack.signingSecret;
     let slackSignature = req.headers['X-Slack-Signature'];
+    let requestBody = qs.stringify(req.body, { format: 'RFC1738' });
     
 }
 
